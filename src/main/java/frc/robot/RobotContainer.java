d30563abab79187b61347c6a17b78aa05db813a4
James <bieszcon29@stu.cedarburg.k12.wi.us>
2026-01-19 20:05:01 -0600

    Fixed inverted joysticks, fixed non-existant submodules being called (Commented out, not deleted)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -143,8 +143,8 @@ public class RobotContainer {
     drive.setDefaultCommand(
         DriveCommands.joystickDrive(
             drive,
-            () -> -controller.getLeftY(),
-            () -> -controller.getLeftX(),
+            () -> controller.getLeftY(),
+            () -> controller.getLeftX(),
             () -> -controller.getRightX()));
 
     // Lock to 0° when A button is held
