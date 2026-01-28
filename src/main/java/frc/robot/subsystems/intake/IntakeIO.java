d6ded11766630403de2417203c27a10d507ff6db
James <bieszcon29@stu.cedarburg.k12.wi.us>
2026-01-28 07:49:12 -0600

    potentially got stuff working

--- src/main/java/frc/robot/subsystems/intake/IntakeIO.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO.java
@@ -1,15 +1,11 @@
 package frc.robot.subsystems.intake;
 
-import static edu.wpi.first.units.Units.Degrees;
-
-import edu.wpi.first.units.measure.Angle;
 import org.littletonrobotics.junction.AutoLog;
 
 public interface IntakeIO {
 
   @AutoLog
   public static class IntakeInputs {
-    public Angle deployAngle = Degrees.of(0);
     public double spinMotorSetSpeed = 0.0;
 
     public double deployMotorSuppliedCurrent = 0.0;
@@ -17,6 +13,9 @@ public interface IntakeIO {
   }
 
   public default void updateInputs(IntakeInputs inputs) {}
+  ;
+
+  public default void configureMotors() {}
   ;
 
   public default Double getSpinMotorSpeed() {
