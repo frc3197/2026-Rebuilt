10a6f3f66efdff0032ac9374661f670ea5eed0d0
James <bieszcon29@stu.cedarburg.k12.wi.us>
2026-01-31 10:43:56 -0600

    Update Deps
    Updated Deps

--- src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
@@ -2,39 +2,39 @@ package frc.robot.subsystems.intake;
 
 public class IntakeIOSim implements IntakeIO {
 
-    Double spinMotorSpeed = 0.0;
-    Double deployMotorSpeed = 0.0;
+  Double spinMotorSpeed = 0.0;
+  Double deployMotorSpeed = 0.0;
 
-    @Override
-    public void updateInputs(IntakeInputs inputs) {
-        inputs.spinMotorSetSpeed = getSpinMotorSpeed();
+  @Override
+  public void updateInputs(IntakeInputs inputs) {
+    inputs.spinMotorSetSpeed = getSpinMotorSpeed();
 
-        // inputs.deployMotorSuppliedCurrent =
-        // deployMotor.getSupplyCurrent().getValueAsDouble();
-        // inputs.spinMotorSuppliedCurrent =
-        // spinMotor.getSupplyCurrent().getValueAsDouble();
-    }
+    // inputs.deployMotorSuppliedCurrent =
+    // deployMotor.getSupplyCurrent().getValueAsDouble();
+    // inputs.spinMotorSuppliedCurrent =
+    // spinMotor.getSupplyCurrent().getValueAsDouble();
+  }
 
-    @Override
-    public void configureMotors() {}
+  @Override
+  public void configureMotors() {}
 
-    @Override
-    public Double getSpinMotorSpeed() {
-        return spinMotorSpeed;
-    }
+  @Override
+  public Double getSpinMotorSpeed() {
+    return spinMotorSpeed;
+  }
 
-    @Override
-    public Double getDeployMotorSpeed() {
-        return deployMotorSpeed;
-    }
+  @Override
+  public Double getDeployMotorSpeed() {
+    return deployMotorSpeed;
+  }
 
-    @Override
-    public void setSpinMotorSpeed(Double speed) {
-       spinMotorSpeed = speed;
-    }
+  @Override
+  public void setSpinMotorSpeed(Double speed) {
+    spinMotorSpeed = speed;
+  }
 
-    @Override
-    public void setDeployMotorSpeed(Double speed) {
-        deployMotorSpeed = speed;
-    }
+  @Override
+  public void setDeployMotorSpeed(Double speed) {
+    deployMotorSpeed = speed;
+  }
 }
